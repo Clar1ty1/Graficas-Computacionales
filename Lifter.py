f7e5f5d11ab3976fb7f2eb9d9fa2b3bf249afeda
Emiliano Berrones <emilianoberrones@gmail.com>
2023-11-30 14:04:11 -0600

    Maniobra evasiva v2
    - Se implemento un modificador de velocidad dependiendo del idx del montacargas

--- Lifter.py
+++ Lifter.py
@@ -32,23 +32,29 @@ A[1,2] = 1;
 A[2,0] = 1;
 
 class Lifter:
-    cargoArea = [-260, 233]
-    weighingScale = [22, 142]
-    machineZone = [48, -88]
     def __init__(self, dim, vel, textures, idx, position, currentNode, other_lifters):
         self.dim = dim
         self.idx = idx
         self.other_lifters = other_lifters
-        self.evasive_time = 5.0
+        self.evasive_time = 50.0
+        self.max_evasive_time = 50.0
+        self.wait_time = 100.0
+        self.max_wait_time = 100.0
         # Se inicializa una posicion aleatoria en el tablero
         # self.Position = [random.randint(-dim, dim), 6, random.randint(-dim, dim)]
         self.Position = position
-        
+        # Inicializar las coordenadas (x,y,z) del cubo en el tablero
+        # almacenandolas en el vector Position
+
+        # Se inicializa un vector de direccion aleatorio
         self.Direction = numpy.zeros(3);
         self.angle = 0
         self.vel = vel
+        self.max_vel = vel
         self.currentNode = currentNode;
         self.nextNode = currentNode;
+        # El vector aleatorio debe de estar sobre el plano XZ (la altura en Y debe ser fija)
+        # Se normaliza el vector de direccion
 
         # Arreglo de texturas
         self.textures = textures
@@ -66,6 +72,8 @@ class Lifter:
         self.trashID = -1
 
     def maniobraEvasiva(self):
+        min_idx_lifter = min(self.idx, *[other_lifter.idx for other_lifter in self.other_lifters])
+        
         for other_lifter in self.other_lifters:
             if other_lifter.idx != self.idx:
                 # Calcular la distancia entre este montacargas y el otro montacargas
@@ -83,10 +91,16 @@ class Lifter:
                     # Normalizar la nueva dirección para asegurar una magnitud adecuada
                     direccion_evasiva /= numpy.linalg.norm(direccion_evasiva)
 
+                    # Si este montacargas tiene el índice más bajo, disminuir la velocidad
+                    if self.idx == min_idx_lifter:
+                        self.vel = self.vel * 0.8  # Disminuir la velocidad en un 20%
+
                     # Actualizar la dirección del montacargas para evitar la colisión
                     return direccion_evasiva
                 else:
                     return None
+
+
 
     def search(self):
         # Change direction randomly
@@ -116,14 +130,28 @@ class Lifter:
             return NodoActual + 1
 
     def update(self, delta):
+
         self.nextNode = self.RetrieveNextNode(self.currentNode);
 		
         Direccion, Distancia =  self.ComputeDirection(self.Position, self.nextNode);
 
         nueva_direccion = self.maniobraEvasiva()
-        if nueva_direccion is not None:
+
+        # Si hay una nueva dirección y estamos dentro del tiempo de evasión
+        if nueva_direccion is not None and self.evasive_time > 0.0:
             # Normalizar la nueva dirección para asegurar una magnitud adecuada
             Direccion = nueva_direccion
+            self.evasive_time -= 1
+
+        # Si estamos dentro del tiempo de espera
+        if self.wait_time > 0.0:
+            self.wait_time -= 1
+        else:
+            # Restablecer el tiempo de espera y el tiempo de evasión
+            self.wait_time = self.max_wait_time
+            self.evasive_time = self.max_evasive_time
+            self.vel = self.max_vel
+
 
         # print(" Agent : %d \t State: %s \t Position : [%0.2f, 0 %0.2f] " %(self.idx, self.status, self.Position[0], self.Position[-1]) );
 
@@ -131,7 +159,7 @@ class Lifter:
             self.currentNode = self.nextNode
 
         mssg = "Agent:%d \t State:%s \t Position:[%0.2f,0,%0.2f] \t NodoActual:%d \t NodoSiguiente:%d" %(self.idx, self.status, self.Position[0], self.Position[-1], self.currentNode, self.nextNode); 
-        #print(mssg);
+        print(mssg);
 
         match self.status:
             case "searching":
@@ -141,8 +169,53 @@ class Lifter:
                 self.angle = math.acos(self.Direction[0]) * 180 / math.pi
                 if self.Direction[2] > 0:
                     self.angle = 360 - self.angle	
-                if Distancia < 1: 
-                    self.currentNode = self.nextNode
+
+                # Move platform
+                if self.platformUp:
+                    if self.platformHeight >= 0:
+                        self.platformUp = False
+                    else:
+                        self.platformHeight += delta
+                elif self.platformDown:
+                    if self.platformHeight <= -1.5:
+                        self.platformUp = True
+                    else:
+                        self.platformHeight -= delta		
+            case "lifting":
+                if self.platformHeight >= 0:
+                    self.targetCenter()
+                    self.status = "delivering"
+                else:
+                    self.platformHeight += delta
+            case "delivering":
+                if (self.Position[0] <= 10 and self.Position[0] >= -10) and (self.Position[2] <= 10 and self.Position[2] >= -10):
+                    self.status = "dropping"
+                else:
+                    newX = self.Position[0] + self.Direction[0] * self.vel
+                    newZ = self.Position[2] + self.Direction[2] * self.vel
+                    if newX - 10 < -self.dim or newX + 10 > self.dim:
+                        self.Direction[0] *= -1
+                    else:
+                        self.Position[0] = newX
+                    if newZ - 10 < -self.dim or newZ + 10 > self.dim:
+                        self.Direction[2] *= -1
+                    else:
+                        self.Position[2] = newZ
+                    self.angle = math.acos(self.Direction[0]) * 180 / math.pi
+                    if self.Direction[2] > 0:
+                        self.angle = 360 - self.angle
+            case "dropping":
+                if self.platformHeight <= -1.5:
+                    self.status = "returning"
+                else:
+                    self.platformHeight -= delta
+            case "returning":
+                if (self.Position[0] <= 20 and self.Position[0] >= -20) and (self.Position[2] <= 20 and self.Position[2] >= -20):
+                    self.Position[0] -= (self.Direction[0] * (self.vel/4))
+                    self.Position[2] -= (self.Direction[2] * (self.vel/4))
+                else:
+                    self.search()
+                    self.status = "searching"
 
 
     def draw(self):
@@ -255,6 +328,24 @@ class Lifter:
         wheel.draw()
         glPopMatrix()
         glDisable(GL_TEXTURE_2D)
+
+        # Lifter
+        glPushMatrix()
+        if self.status in ["lifting","delivering","dropping"]:
+            self.drawTrash()
+        glColor3f(0.0, 0.0, 0.0)
+        glTranslatef(0, self.platformHeight, 0)  # Up and down
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3d(1, 1, 1)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3d(1, 1, -1)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3d(3, 1, -1)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3d(3, 1, 1)
+        glEnd()
+        glPopMatrix()
         glPopMatrix()
 
     def drawTrash(self):
